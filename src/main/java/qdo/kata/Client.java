f325361b385d1090aa767fbfa480c84fd3c7a711
Quentin <qdorme@gmail.com>
2021-10-04 20:24:42 +0200

    [US1][TEST] Amount has to be greater to 0 - OK

--- src/main/java/qdo/kata/Client.java
+++ src/main/java/qdo/kata/Client.java
@@ -3,6 +3,8 @@ package qdo.kata;
 public class Client {
 
 	public Client deposeMoney(double amount) {
+		if (amount < 0.01)
+			throw new IllegalArgumentException("The amount of a deposit has to be at least 0.01");
 		return this;
 	}
 }
